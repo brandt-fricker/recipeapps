57a96fad72d0e0728244f21b578dc4881acaefc8
Ricky Justiniano <justiniano.ricky@gmail.com>
2020-12-17 11:57:36 -0700

    password function

--- server.js
+++ server.js
@@ -5,22 +5,7 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+
 
 // Sets up the Express App
 // =============================================================
@@ -48,6 +33,7 @@ app.use(express.static("public"));
 require("./routes/htmlroutes.js")(app);
 require("./routes/user-apiroutes.js")(app);
 require("./routes/recipe-apiroutes.js")(app);
+require("./routes/userlogin-apiroutes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
