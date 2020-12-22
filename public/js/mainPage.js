dcd2b2cec87d5ecdaff18bed2aac6d70ce79ac50
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-21 21:53:30 -0700

    cleanedup the code

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,24 +1,7 @@
 $(document).ready(function () {
   const getRandomBtn = $("#getRandom");
   const getAllBtn = $(".getAll");
-  const getSpecificBtn = $(".getSpecific");
-  const addYourBtn = $(".addYour");
-  const updateYourBtn = $(".updateYour");
-  const deleteYourBtn = $(".deleteYour");
-  const saveRecipe = $(".saveBtn");
-  const titleInput = $("#foodTitle");
-  const descrInput = $("#foodDecsript");
-  const mealInput = $("#foodType");
-
-  // getRandomBtn.on("click", function(e) {
-  //     e.preventDefault();
-
-  //     $.ajax("/api/get-random", {
-  //         method: "GET",
-  //     }).then(function (data) {
-
-  //     });
-  // });
+  const deleteBtn = $(".deleteBtn");
 
   getAllBtn.on("click", function (e) {
     e.preventDefault();
@@ -26,13 +9,14 @@ $(document).ready(function () {
     $.ajax("api/recipes", {
       type: "GET",
     }).then(function (data) {
-      console.log(data);
       for (let i = 0; i < data.length; i++) {
+
+        let idOfElement = data[i].id;
         let title = $("<p>")
           .attr("class", "foodDataTitle")
           .attr("id", "foodTitle")
           .text(`Recipe name - ${data[i].title}`);
-        console.log(data[i].title);
+        
 
         let ingredients = $("<p>")
           .attr("class", "foodDataIngred")
@@ -49,54 +33,38 @@ $(document).ready(function () {
           .attr("id", "foodType")
           .attr("src", data[i].mealType);
 
+        let deleteBtn = $("<button>")
+          .attr("class", "deleteBtn")
+          .text("Delete Recipe");
+
+          
+
         $(".recipe-container").append(
           $("<div>")
             .attr("class", "recipeDiv")
-            .append(mealType, title, ingredients, description)
+            .append(mealType, title, ingredients, description, deleteBtn)
         );
+        deleteBtn.on("click", function (e) {
+          // Make sure to preventDefault on a submit event.
+        
+          console.log("Oops", idOfElement);
+      
+          const id = idOfElement;
+          
+      
+          // Send the DELETE request.
+          $.ajax({
+            url: "/api/delete-recipe/" + id,
+            method: "DELETE",
+          }).then(function () {
+            console.log("Deleted!");
+            // Reload the page to get the updated list
+            window.location.href = "/main";
+          });
+        });
+        console.log(idOfElement);
       }
     });
+    });
 
-
-    
-  });
-
-//   saveRecipe.on("click", function (e) {
-//     e.preventDefault();
-//     $.get("/api/user_data").then(function (data) {
-//       userId = data.id;
-//       console.log(userId);
-//     });
-
-//     const { title, description, mealType, UserId } = {
-//       title: titleInput.val().trim(),
-//     //   ingredients: ingrInput.val().trim(),
-//       description: descrInput.val().trim(),
-//       mealType: mealInput.val().trim(),
-//       UserId: userId,
-//     };
-// // console.log(title, description, mealType, UserId)
-
-//     if (!title || !description || !mealType || !UserId) {
-//       return;
-//     }
-//     pushData(title, description, mealType, UserId);
-
-//     titleInput.val("");
-//     // ingrInput.val("");
-//     descrInput.val("");
-//     mealInput.val("");
-
-//     function pushData(title, description, mealType, UserId) {
-//       $.post("/api/save-recipes", {
-//         title: title,
-//         // ingredients: ingredients,
-//         description: description,
-//         mealType: mealType,
-//         UserId: userId,
-//       }).then(function (data) {
-//         console.log(data, "Success!");
-//       });
-//     }
-//   });
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,77 +1,66 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+const path = require("path");
 const axios = require("axios");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-
-  app.get("/", function(req, res) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      // res.redirect("/members");
       res.render("index");
     }
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+
     res.render("signup");
   });
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      // res.redirect("/members");
       res.render("index");
     }
-    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
     res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    // res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/members", isAuthenticated, function (req, res) {
     res.render("index");
   });
 
   app.get("/get-random", isAuthenticated, async function (req, res) {
     let queryUrl =
       "https://api.spoonacular.com/recipes/random?number=1&apiKey=ba3cef0a320c41c5bbd79cbab4cf8d92";
-    
-      let hbsObject = {
-      
-        title: "",
-        ingredients: '',
-        description: '',
-        mealType: '',
-     
-        
-      };
+
+    let hbsObject = {
+      title: "",
+      ingredients: "",
+      description: "",
+      mealType: "",
+    };
 
     await axios.get(queryUrl).then(function (response) {
+      hbsObject.title += response.data.recipes[0].title;
+      hbsObject.ingredients += response.data.recipes[0].extendedIngredients.map(
+        ({ original }) => original
+      );
+      hbsObject.description += response.data.recipes[0].instructions + " , ";
+      hbsObject.mealType += response.data.recipes[0].image;
 
-        hbsObject.title += response.data.recipes[0].title;
-        hbsObject.ingredients += response.data.recipes[0].extendedIngredients.map(({original}) => original);;
-        hbsObject.description += response.data.recipes[0].instructions + " , ";
-        hbsObject.mealType += response.data.recipes[0].image;
-
-    console.log(response)
-console.log(hbsObject.ingredients)
+      console.log(response);
+      console.log(hbsObject.ingredients);
     });
     console.log(hbsObject, "hello");
     res.render("recipe", hbsObject);
   });
 
-  app.get("/main", isAuthenticated, function(req, res) {
-    // res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/main", isAuthenticated, function (req, res) {
     res.render("index");
   });
 
-  app.get("/addRecipe", isAuthenticated, function(req, res) {
-    // res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/addRecipe", isAuthenticated, function (req, res) {
     res.render("addRecipe");
   });
-
-
-
 };
